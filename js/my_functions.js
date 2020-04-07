033b07fa0fb7f8ec0b62ad58449ee0521bacd276
mesuttopacik <mesuttopacik@gmail.com>
2020-04-07 16:57:27 +0300

    myPerWebSite

---
+++ js/my_functions.js
@@ -0,0 +1,24 @@
+
+    const proje = () =>{
+      alert("React,Redux,Thunk.. gibi teknolojiler kullanarak hazırladığım proje bitince yüklenecek.");//projeler butonu
+    }
+ 
+    //for slider
+
+    // $('.carousel.carousel-multi-item.v-2 .carousel-item').each(function(){
+    //     var next = $(this).next();
+    //     if (!next.length) {
+    //       next = $(this).siblings(':first');
+    //     }
+    //     next.children(':first-child').clone().appendTo($(this));
+      
+    //     for (var i=0;i<4;i++) {
+    //       next=next.next();
+    //       if (!next.length) {
+    //         next=$(this).siblings(':first');
+    //       }
+    //       next.children(':first-child').clone().appendTo($(this));
+    //     }
+    //   });
+ 
+    //slider dan vazgeçtim
